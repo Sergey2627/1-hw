e7e89f512a4dfe353c84e88f72a9861e2c01f7e1
Serg <sergejancev26@gmail.comgit config --listgit config --global user.email sergejancev26@gmail.com>
2023-09-19 10:53:42 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// alert('Зовнішній файл');
+
+// console.log('External file');
+
+// console.log('Hello, world!');
+
+// // document.write('Some String');
+// console.log(1234);
+// console.log('1234');
+
+
+// x = 333;
+
+// console.log(x);
+
+// stroka = 'any string';
+
+// console.log(stroka);
+
+// alert('Alert');
+
+num = prompt('Enter your name:');
+alert("Hello, " + num + "! How are you?");
